aae1ca4310d640f9e87e1122842f9ff0bd1bb326
wlovcc <wolflovcc@gmail.com>
2025-07-05 01:51:06 +0800

    Create curd.sql

---
+++ Task_03/sql/curd.sql
@@ -0,0 +1,4 @@
+INSERT INTO students (name, age, grade) VALUES ('张三', 20, '三年级');
+SELECT * FROM students WHERE age > 18;
+UPDATE students SET grade = '四年级' WHERE name = '张三';
+DELETE FROM students WHERE age < 15;
